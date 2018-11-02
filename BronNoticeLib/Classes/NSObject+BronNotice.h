bc7d9fc6e2d6d8e51a929e663d8b1ddf62b4dece
hbj520 <hbjmailformac@163.com>
2018-11-02 17:42:52 +0800

    添加noticeView--han

---
+++ BronNoticeLib/Classes/NSObject+BronNotice.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+BronNotice.h
+//  BronNoticeLib
+//
+//  Created by Bron on 2018/11/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (BronNotice)
++ (void)addView;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BronNoticeLib/Classes/NSObject+BronNotice.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+BronNotice.m
+//  BronNoticeLib
+//
+//  Created by Bron on 2018/11/2.
+//
+
+#import "NSObject+BronNotice.h"
+
+@implementation NSObject (BronNotice)
++ (void)addView{
+    NSLog(@"print something");
+}
+@end
